7ca024ede2ccd33fa1fab4ec4a73c67d5b147022
Egor Pak <goshkis@gmail.com>
2021-02-21 12:52:50 -0800

    Add comments for values

--- config.py
+++ config.py
@@ -4,12 +4,13 @@
 QUEUE_NAME = 'q1_standard'
 
 # SQS queue parameters
-VISIBILITY_TIMEOUT_S = 15
-MAX_MSG_SIZE_BYTES = 2048
-RETENTION_PERIOD_S = 900
+VISIBILITY_TIMEOUT_S = 5  # One msg estimated processing time during normal operations (0.1) *
+                          # BATCH_SIZE (10) * acceptable delay (5x from normal time - random for now)
+MAX_MSG_SIZE_BYTES = 2048 # Value suitable for testing
+RETENTION_PERIOD_S = 900  # Value suitable for testing
 
 # SQS ReceiveMessage parameters.
-BATCH_SIZE = 10
+BATCH_SIZE = 10   # Keep in mind visibility timeout and single msg processing time
 MAX_WAIT_TIME = 2 # non-zero means long pool
 
 # Extra logging for message reader
