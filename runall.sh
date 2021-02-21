4cd9b96a72944e2c4e4ff40ecf1cfa63c4de1302
Egor Pak <goshkis@gmail.com>
2021-02-20 21:44:40 -0800

    Add base-index awareness. Add client start delay.

--- runall.sh
+++ runall.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-tmux new-session -d bash
-tmux split-window -h bash
-tmux send -t 0:1.1 "python3 ./server.py" C-m
-tmux send -t 0:1.2 "python3 ./worker.py" C-m
-tmux -2 attach-session -d
+SESS=egorsqs
+
+tmux new-session -d -s $SESS
+BASE=$(tmux display-message -p "#{base-index}")
+BASEP=$(tmux display-message -p "#{pane-base-index}")
+tmux split-window -h
+tmux split-window -h
+tmux select-layout even-horizontal
+tmux send -t $SESS:$BASE.$BASEP "python3 ./server.py" C-m
+tmux send -t $SESS:$BASE.$(($BASEP+1)) "python3 ./worker.py" C-m
+# client will start with 5s delay to allow service to start
+tmux send -t $SESS:$BASE.$(($BASEP+2)) "sleep 5;python3 ./client.py" C-m
+tmux -2 attach-session -d -t egorsqs
