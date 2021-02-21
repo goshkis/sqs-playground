65fb450c7f778559c6f2c52a266e76115c3ae12b
Egor Pak <goshkis@gmail.com>
2021-02-20 23:54:55 -0800

    Fix instance of hardcoded session name

--- runall.sh
+++ runall.sh
@@ -12,4 +12,4 @@ tmux send -t $SESS:$BASE.$BASEP "python3 ./server.py" C-m
 tmux send -t $SESS:$BASE.$(($BASEP+1)) "python3 ./worker.py" C-m
 # client will start with 5s delay to allow service to start
 tmux send -t $SESS:$BASE.$(($BASEP+2)) "sleep 5;python3 ./client.py" C-m
-tmux -2 attach-session -d -t egorsqs
+tmux -2 attach-session -d -t $SESS
